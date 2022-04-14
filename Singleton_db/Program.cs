05b2e189db87f68db7c31d7768ab402be00a842c
idunnoaiii <idunno.aiii@gmail.com>
2022-04-14 22:52:40 +0700

    a

---
+++ Singleton_db/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Autofac;
+using NUnit.Framework;
+using Singleton_db;
+
+var db = SingletonDatabase.Instance;
+
+ContainerBuilder builder = new ContainerBuilder();
+
+Console.WriteLine(db.GetPopulation("My"));
+
+Console.WriteLine("Hello, World!");
---
+++ Singleton_db/SingletonDatabase.cs
@@ -0,0 +1,33 @@
+using MoreLinq;
+
+namespace Singleton_db;
+
+public interface IDatabase
+{
+    int GetPopulation(string name);
+}
+
+public class SingletonDatabase : IDatabase
+{
+
+    private readonly Dictionary<string, int> _populations;
+    
+    private static Lazy<SingletonDatabase> _instanceLazy =  new Lazy<SingletonDatabase>(() =>new SingletonDatabase());
+
+    private SingletonDatabase()
+    {
+        _populations = File.ReadAllLines("capitals.txt")
+            .Batch(2)
+            .ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1))
+            );
+    }
+    
+    public int GetPopulation(string name)
+    {
+        return _populations[name];
+    }
+
+    public static SingletonDatabase Instance => _instanceLazy.Value;
+}
